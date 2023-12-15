74b84d0d6e86a85d5ad4e2a2496b86790b2514bc
Jennie <jguil429@gmail.com>
2023-12-15 08:43:18 -0600

    added random color function

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+const button = document.querySelector('button');
+button.addEventListener('click', function () {
+    const newColor = makeRandomColor();
+    document.body.style.backgroundColor = newColor;
+});
+
 const makeRandomColor = () => {
     const r = Math.floor(Math.random() * 255);
     const g = Math.floor(Math.random() * 255);
